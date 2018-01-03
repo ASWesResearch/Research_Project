4482e067caa9b9d3431076925259b4f92121a608
ASWesResearch <32938382+ASWesResearch@users.noreply.github.com>
2018-01-03 17:44:37 -0500

    Testing Master

--- Test_Master_Code.py
+++ Test_Master_Code.py
@@ -2,6 +2,7 @@ import threading
 from threading import Thread
 import os
 from os import system
+import sys
 def func1():
     print 'Working'
 def func2():
@@ -10,10 +11,20 @@ def func2():
     from os import system
     #os.system('python Hello_World.py')
     dir = os.path.dirname(__file__)
-    path=os.path.realpath('../GitHub/Galaxy_Histogram_Code_2.py')
+    #path=os.path.realpath('../GitHub/Galaxy_Histogram_Code_2.py')
+    path=os.path.realpath('../')
     print "Path=",path
     system('pwd')
-    os.system('python '+path)
+    #os.system('python '+path) #This works when inputs are not used
+    #import Galaxy_Histogram_Code_2
+    #Driver_Code('NGC4258')
+    sys.path.append(os.path.abspath(path))
+    print sys.path
+    #from Galaxy_Histogram_Code_2 import *
+    #import Galaxy_Histogram_Code_2
+    from Histogram_Code import Galaxy_Histogram_Code_2
+    Galaxy_Histogram_Code_2.Driver_Code('NGC4258')
+    #print "Parinoia Test"
 if __name__ == '__main__':
     Thread(target = func1).start()
     Thread(target = func2).start()
